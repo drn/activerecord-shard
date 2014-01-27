afaff54548bd107755b8aa416149e4087545fbbd
darrenli <darren@thanx.com>
2014-01-27 00:43:40 -0800

    Add initial travis configuration.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm activerecord-dynamic-*.gem 2>/dev/null
 gem build activerecord-dynamic.gemspec
-gem install activerecord-dynamic-*.gem -- development
+gem install activerecord-dynamic-*.gem --development
