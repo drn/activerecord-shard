e27aab30755f5767e1a826aee85a576a1a1d6ab8
darrenli <darren@thanx.com>
2014-01-29 00:05:30 -0800

    Add skeleton spec files for Errors, Schema, Validator.

---
+++ spec/activerecord/dynamic/errors_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../spec_helper.rb'
+
+describe 'ActiveRecord::Dynamic Errors' do
+
+  let(:core){ ActiveRecord::Dynamic::Core }
+  let(:schema){ ActiveRecord::Dynamic::Schema }
+  let(:validator){ ActiveRecord::Dynamic::Validator }
+
+end
---
+++ spec/activerecord/dynamic/schema_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../spec_helper.rb'
+
+describe ActiveRecord::Dynamic::Schema do
+
+  let(:core){ ActiveRecord::Dynamic::Core }
+  let(:schema){ ActiveRecord::Dynamic::Schema }
+  let(:validator){ ActiveRecord::Dynamic::Validator }
+
+end
